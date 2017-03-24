3899772d151633e473cffe28abd9f14dc3f11527
erangad <erangadulshan.14@cse.mrt.ac.lk>
2017-03-24 19:53:22 +0530

    sign Up form

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import {Router, Route, browserHistory} from 'react-router';
 import {render} from 'react-dom';
 import {Header} from "./components/Header";
+import {Login} from "./components/Login";
+import {SignUp} from "./components/SignUp";
 class App extends React.Component{
     render(){
         return(
@@ -8,6 +11,13 @@ class App extends React.Component{
                 <div className="row">
                     <Header/>
                 </div>
+                <div className="row">
+                    <Checker/>
+                </div>
+                <Router history={browserHistory}>
+                    <Route path={"login"} component={Login}/>
+                    <Route path={"signUp"} component={SignUp}/>
+                </Router>
             </div>
         );
 
